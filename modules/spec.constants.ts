5c0ac732b2df22d9a1b9bb654c51152315332c77
Cody Dalton <codyjdalton@gmail.com>
2018-09-06 21:49:15 -0700

    spec-integ: remove default datasource branch

--- modules/spec.constants.ts
+++ modules/spec.constants.ts
@@ -1,6 +1,6 @@
 
 export const SpecConstants = {
     
-    DATASOURCE_INTEG: process.env.DATASOURCE_INTEG || 'mongodb://localhost:<port>/<project-integ',
+    DATASOURCE_INTEG: process.env.DATASOURCE_INTEG,
     DEFAULT_CONTENT: '; charset=utf-8'
 };
